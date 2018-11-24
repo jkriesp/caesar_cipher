4c83b7e0170b00985c85ed03f4dbe9b888499f1b
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-24 11:27:19 +0100

    clean up

--- substrings.rb
+++ substrings.rb
@@ -14,6 +14,4 @@ def substrings(string, dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-
