27cca342d21581f8c09bd2120df75918b1ccaefe
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-27 19:00:25 +0100

    bubble sort accepting block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,4 +12,24 @@ def bubble_sort(arr)
   return arr  
 end
 
+
+
+def bubble_sort_by(arr)
+  swap = true
+  while swap
+    swap = false
+    (arr.length - 1).times do |x|
+      if yield(arr[x], arr[x+1]) > 0
+        arr[x], arr[x+1] = arr[x+1], arr[x]
+        swap = true
+      end
+    end
+  end
+  p arr  
+end
+
 p bubble_sort([4,3,78,2,0,2])
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
